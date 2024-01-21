c8f1c62489f970c32474ade5f7bdf6a4a9429270
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2024-01-21 18:39:42 +0900

    Create JyankenExec_Chapter24.java

---
+++ JyankenExec_Chapter24.java
@@ -0,0 +1,8 @@
+package kadai_024;
+
+public class JyankenExec_Chapter24 {
+    public static void main(String[] args) {
+        Jyanken_Chapter24 jyankenGame = new Jyanken_Chapter24();
+        jyankenGame.playGame();
+    }
+}
