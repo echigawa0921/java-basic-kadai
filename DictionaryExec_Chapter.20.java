2c983f5ab9d0443425428862898b131e1cd01bcd
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2024-01-27 17:14:01 +0900

    Create DictionaryExec_Chapter.20.java

---
+++ DictionaryExec_Chapter.20.java
@@ -0,0 +1,15 @@
+package kadai_20;
+
+public class DictionaryExec_Chapter {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter20 dictionary = new Dictionary_Chapter20();
+		
+		dictionary.searchWord("apple");
+		dictionary.searchWord("banana");
+		dictionary.searchWord("grape");
+		dictionary.searchWord("orange");
+
+	}
+
+}
