f8795c4340e1ef8ac91cd21aef01c844ae912fdd
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2024-01-27 16:06:53 +0900

    Create KatoTaro_Chapter17.java

---
+++ KatoTaro_Chapter17.java
@@ -0,0 +1,14 @@
+package kadai_017;
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+	public KatoTaro_Chapter17() {
+		familyName = "加藤";
+		givenName = "太郎";
+		address = "東京";				
+	}
+	
+	@Override
+	public void eachIntroduce() {
+		System.out.println("私はJavaが好きです。");
+	}
+}
