7033717d583fe2261d692aa200786bf15ab31ba7
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2024-01-27 16:07:38 +0900

    Create KatoExec_Chapter17.java

---
+++ KatoExec_Chapter17.java
@@ -0,0 +1,24 @@
+package kadai_017;
+
+public class KatoExec_Chapter17 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
+		KatoIchiro_Chapter17 ichiro = new KatoIchiro_Chapter17();
+		KatoHanako_Chapter17 hanako = new KatoHanako_Chapter17();
+		
+		System.out.println("太郎の紹介:");
+		taro.execIntroduce();
+		System.out.println();
+		
+		System.out.println("一郎の紹介:");
+		ichiro.execIntroduce();
+		System.out.println();
+		
+		System.out.println("花子の紹介");
+		hanako.execIntroduce();
+		System.out.println();
+	}
+
+}
