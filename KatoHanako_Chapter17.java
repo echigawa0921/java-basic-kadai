8f40f70194a1dad05df5a2f0ea833205ce4bbbd1
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2024-01-27 16:07:23 +0900

    Create KatoHanako_Chapter17.java

---
+++ KatoHanako_Chapter17.java
@@ -0,0 +1,15 @@
+package kadai_017;
+
+public class KatoHanako_Chapter17 extends Kato_Chapter17 {
+	public KatoHanako_Chapter17() {
+		familyName = "加藤";
+		givenName = "花子";
+		address = "京都府";
+	}
+	
+	@Override
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です。");
+	}
+
+}
